f8c66bd7494ca85c438f89d8da97b385b39d1be0
Francesco Stablum <stablum@gmail.com>
2013-09-17 19:35:17 +0200

    iterator: to be tested

--- src/environment/StateSpace.java
+++ src/environment/StateSpace.java
@@ -1,10 +1,13 @@
 package environment;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class StateSpace implements Iterable<State> {
+public class StateSpace implements Iterable<State>, Iterator<State> {
 
 	private State[][][][] states;
+	
+	private int iter_pos = 0;
 	
 	public StateSpace() { this.initStates(); }
 
@@ -20,7 +23,7 @@ public class StateSpace implements Iterable<State> {
 		}
 	}
 	
-	public void setStateSpace(int i, int j, int k, int l, double stateValue) {
+	public void setStateValue(int i, int j, int k, int l, double stateValue) {
 		this.states[i][j][k][l].setStateValue(stateValue);
 	}
 	
@@ -30,6 +33,38 @@ public class StateSpace implements Iterable<State> {
 
 	@Override
 	public Iterator<State> iterator() {
-		return null;
+		this.resetIterator();
+		return this;
+	}
+
+	private void resetIterator() {
+		this.iter_pos = 0;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return this.iter_pos < (Util.DIM^4);
+	}
+
+	@Override
+	public State next() {
+		if(this.hasNext()){
+			int tmp = this.iter_pos;
+			int l = tmp % Util.DIM;
+			tmp = (int)(tmp / Util.DIM);
+			int k = tmp % Util.DIM;
+			tmp = (int)(tmp / Util.DIM);
+			int j = tmp % Util.DIM;
+			tmp = (int)(tmp / Util.DIM);
+			int i = tmp % Util.DIM;
+			return this.states[i][j][k][l];
+		}
+		throw new NoSuchElementException();
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
 	}
 }
