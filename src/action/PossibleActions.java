0c1b8eeac05c755ce75950cb0df59520aa0fe64c
unknown <6200982@u030146.uva.nl>
2013-09-21 14:36:11 +0200

    PossibleAction : some documentation

--- src/action/PossibleActions.java
+++ src/action/PossibleActions.java
@@ -4,6 +4,19 @@ import environment.Environment.action;
 
 import java.util.HashMap;
 import java.util.Iterator;
+
+/**
+ * this class represents a specific part of a policy: 
+ * 	the association between *a single state* and a set of possible actions
+ *  that might be good for that particular state.
+ *  Each of the specified actions has a probability of choice associated.
+ *  This means that the agent will choose randomly and according to
+ *  that probability distribution an action.
+ *  Please note that a deterministic choice CAN be implemented with this system:
+ *  it's just enough to set the probability of an action to 1 and the other actions to 0.
+ * @author Francesco Stablum
+ *
+ */
 public abstract class PossibleActions {
 	
 	protected HashMap<action, Double> actionProbability;
