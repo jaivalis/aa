517ad0ee4ace9d75c76bd83e82afb4b23cb8378b
Francesco Stablum <stablum@gmail.com>
2013-09-30 23:04:43 +0200

    bugfix and print Q

--- src/environment/Q.java
+++ src/environment/Q.java
@@ -6,10 +6,12 @@ import state.State;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Q {
 	HashMap<StateAction,Double> sa_d = new HashMap<StateAction,Double>();
-	HashMap<State, ArrayList<StateAction>> s_sa = new HashMap<State,ArrayList<StateAction>>();
+	HashMap<State, HashSet<StateAction>> s_sa = new HashMap<State,HashSet<StateAction>>();
 
     /**
      * Sets the Q value of <state, action> to a given value.
@@ -22,12 +24,12 @@ public class Q {
 		this.sa_d.put(sa, d);
 		
 		// structure for cheap state-action retrieval
-		ArrayList<StateAction> sa_list = this.s_sa.get(s);
-		if(sa_list == null){
-			sa_list = new ArrayList<StateAction>();
+		HashSet<StateAction> sa_set = this.s_sa.get(s);
+		if(sa_set == null){
+			sa_set = new HashSet<StateAction>();
 		}
-        sa_list.add(sa);
-		this.s_sa.put(s, sa_list);
+        sa_set.add(sa);
+		this.s_sa.put(s, sa_set);
 	}
 
     /**
@@ -35,9 +37,9 @@ public class Q {
      * @param s; State in question
      * @return ArrayList<StateAction>; List of StateActions
      */
-	public ArrayList<StateAction> getStateActions(State s) {
-		ArrayList<StateAction> sa_list = this.s_sa.get(s);
-		return sa_list;
+	public HashSet<StateAction> getStateActions(State s) {
+		HashSet<StateAction> sa_set = this.s_sa.get(s);
+		return sa_set;
 	}
 
     /**
@@ -47,8 +49,8 @@ public class Q {
      * @return double; Q value.
      */
 	public double get(State s, action a) {
-		ArrayList<StateAction> sa_list = this.getStateActions(s);
-		for(StateAction sa : sa_list) {
+		HashSet<StateAction> sa_set = this.getStateActions(s);
+		for(StateAction sa : sa_set) {
 			if(sa.getA().equals(a)) {
 				return this.sa_d.get(sa);
 			}
@@ -63,10 +65,10 @@ public class Q {
      * @return action; The optimal action.
      */
 	public action getArgmaxA(State s) {
-		ArrayList<StateAction> sa_list = this.getStateActions(s);
+		HashSet<StateAction> sa_set = this.getStateActions(s);
 		double max = Double.NEGATIVE_INFINITY;
 		action argmax_a = null;
-		for(StateAction sa : sa_list) {
+		for(StateAction sa : sa_set) {
 			Double val = this.sa_d.get(sa);
 			if(val > max) {
 				max = val;
@@ -81,13 +83,25 @@ public class Q {
      * @return double; The max Q value.
      */
 	public double getMax(State s) {
-		ArrayList<StateAction> sa_list = this.getStateActions(s);
+		HashSet<StateAction> sa_set = this.getStateActions(s);
 		double max = Double.NEGATIVE_INFINITY;
-		for(StateAction sa : sa_list) {
+		for(StateAction sa : sa_set) {
 			Double val = this.sa_d.get(sa);
 			if(val > max) {
 				max = val;
 			}
 		} return max;
+	}
+
+	public void print() {
+		for(State s : this.s_sa.keySet()){
+			System.out.println(s);
+			HashSet<StateAction> sa_set = this.s_sa.get(s);
+			for(StateAction sa : sa_set) {
+				action a = sa.getA();
+				Double d = this.sa_d.get(sa);
+				System.out.println(s+":"+a+":"+d);
+			}
+		}
 	}
 }
