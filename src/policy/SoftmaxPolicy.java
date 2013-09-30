09a147e4a07058c4c77a5c9f179a2a97daf32627
jaivalis <johnaivalis@gmail.com>
2013-09-30 22:41:07 +0200

    working on softmaxP

--- src/policy/SoftmaxPolicy.java
+++ src/policy/SoftmaxPolicy.java
@@ -10,10 +10,10 @@ import statespace.StateSpace;
 /**
  * SoftMax policy tries to vary the action probabilities as a graded function of estimated value. The greedy action
  * is still given the highest selection probability, but all the others are ranked and weighted according to their
- * value estimates. These are called softmax action selection rules. The most common softmax method uses a Gibbs, or
+ * value estimates. These are called SoftMax action selection rules. The most common SoftMax method uses a Gibbs, or
  * Boltzmann, distribution. It chooses action 'a' the t-th play with probability:
- * exp(Q(a) / tau) / sum{ exp(Q(b) / tau) }
  *
+ * exp(Q_t(a) / tau) / sum{ exp(Q_t(b) / tau) }
  * tau = temperature
  */
 public class SoftmaxPolicy extends PredatorPolicy {
@@ -29,13 +29,27 @@ public class SoftmaxPolicy extends PredatorPolicy {
 
     @Override
     public Environment.action getAction(State s) {
+        PossibleActions possibleActions = this.stateActionMapping.get(s);
 
-        if(this.q == null) {
-            new Exception("SoftmaxPolicy: this.q is not set. Please call setQ before!").printStackTrace();
-            System.exit(0);
+        double numerator, denominator, prob;
+        for (PossibleActions pa : possibleActions) {
+
+            numerator = Math.exp();
+
+            denominator = 0;
+            prob = numerator / denominator;
+            pa.setActionProbability(prob);
         }
-        // TODO
 
-        return null;
+        // set all probabilities to epsilon divided by number of actions
+
+
+        // find maximum action and set it to the greedy probability value (1 - epsilon + epsilon/size(A))
+        Environment.action max_a = this.q.getArgmaxA(s);
+        double greedy_prob = 1 - Util.epsilon + epsilon_frac;
+        possibleActions.setActionProbability(max_a, greedy_prob);
+
+        // stochastic query to get action for state s
+        return super.getAction(s);
     }
 }
