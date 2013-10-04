ab10fe82c4200c55aea5218bb6cf766fd069b8e5
Francesco Stablum <stablum@gmail.com>
2013-10-04 19:21:42 +0200

    discounted

--- src/episode/Episode.java
+++ src/episode/Episode.java
@@ -1,6 +1,7 @@
 package episode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 import environment.Algorithms.action;
@@ -16,6 +17,18 @@ public class Episode implements Iterable<EpisodeStep> {
 	@Override
 	public Iterator<EpisodeStep> iterator() {
 		return this.steps.iterator();
+	}
+	
+	public void refreshDiscounted(double gamma) {
+		ArrayList<EpisodeStep> reverted = (ArrayList<EpisodeStep>) this.steps.clone();
+		Collections.reverse(reverted);
+		double prev_discounted = 0.0;
+		for(EpisodeStep step : reverted) {
+			double curr_discounted = step.getR() + gamma * prev_discounted;
+			step.setDiscounted(curr_discounted);
+			prev_discounted = curr_discounted;
+		}
+		
 	}
 
 }
