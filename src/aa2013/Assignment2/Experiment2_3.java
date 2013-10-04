65b7cdbac2459fc13b9cbaee9ddc2aaebb9f8a15
TopSteely <tobias.stahl1@gmx.de>
2013-10-04 22:00:41 +0200

    main 2_3

--- src/aa2013/Assignment2/Experiment2_3.java
+++ src/aa2013/Assignment2/Experiment2_3.java
@@ -14,7 +14,7 @@ import statespace.StateSpace;
 public class Experiment2_3 {
 
     /**
-     * Experiment on Sarsa:
+     * Experiment on Softmax:
      *      + different α learning rates.
      *      + different γ discount factors.
      *  - epsilon is set to 0.1
@@ -27,14 +27,16 @@ public class Experiment2_3 {
         Algorithms algos = new Algorithms(ss);
 
         double optimisticInitialQ = 15;
+        double gamma = 0.9;
+        double alpha = 0.5;
         double simulations = 100;  // many simulations ensure higher precision.
         SoftmaxPolicy smp = new SoftmaxPolicy(algos.getStateSpace()); // Predator learn
 
         double savedEpsilon = Util.epsilon; // FIXME: dirty hack!
-        for (double gamma = 0.1; gamma <= 1.0; gamma += 0.2) {
-        	for (double alpha = 0.1; alpha <= 0.6; alpha += 0.1) {
-               	for(int episodeCount = 0; episodeCount < Util.EPISODE_COUNT; episodeCount += 50) {
+        	for (double tau = 0.1; tau <= 0.9; tau += 0.1) {
+               	for(int episodeCount = 50; episodeCount < Util.EPISODE_COUNT; episodeCount += 50) {
             		// 1. train
+               		Util.tau = tau;
             		Util.epsilon = savedEpsilon; // we need a stochastic epsilon policy for the learning, for exploration
             		Q newQ = algos.Q_Learning(smp, optimisticInitialQ, alpha, gamma, episodeCount);
             		Util.epsilon = 0.0; // now it has already learned, so we can use a stochastic policy
@@ -42,9 +44,8 @@ public class Experiment2_3 {
 
             		// 2. simulate & output results
             		double averageRounds = algos.getSimulationAverageRounds(simulations);
-            		System.out.print(averageRounds + " & ");
+            		System.out.println(tau +", "+averageRounds);
             	}
             } System.out.println("\\\\");
         }
-    }
 }
